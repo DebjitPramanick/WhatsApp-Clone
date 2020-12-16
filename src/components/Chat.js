fe0b6bdfd25bd7376b51788d858374cc986faf41
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-16 10:22:02 +0530

    Version 1

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,6 +2,8 @@ import { Avatar, IconButton } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import {SearchOutlined} from '@material-ui/icons';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 
 import React from 'react'
 import "../styles/Chat.css";
@@ -55,77 +57,22 @@ const Chat = () => {
                     </span>
                 </p>
 
-                <p className="chat-message">
-                    <span className="chat-name">Debjit</span>
-                    This is message.
-                    <span className="chat-timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
+            </div>
 
-                <p className="chat-message">
-                    <span className="chat-name">Debjit</span>
-                    This is message.
-                    <span className="chat-timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
+            <div className="chat-footer">
+                <InsertEmoticonIcon/>
 
-                <p className="chat-message">
-                    <span className="chat-name">Debjit</span>
-                    This is message.
-                    <span className="chat-timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
+                <form>
+                    <input 
+                    placeholder="Type message .."
+                    type="text"/>
+                    <button
+                    type="submit">
+                        Send a message
+                    </button>
+                </form>
 
-                <p className="chat-message chat-reciever">
-                    <span className="chat-name">Debjit</span>
-                    This is message.
-                    <span className="chat-timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-
-                <p className="chat-message chat-reciever">
-                    <span className="chat-name">Debjit</span>
-                    This is message.
-                    <span className="chat-timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-
-                <p className="chat-message chat-reciever">
-                    <span className="chat-name">Debjit</span>
-                    This is message.
-                    <span className="chat-timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-
-                <p className="chat-message chat-reciever">
-                    <span className="chat-name">Debjit</span>
-                    This is message.
-                    <span className="chat-timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-
-                <p className="chat-message chat-reciever">
-                    <span className="chat-name">Debjit</span>
-                    This is message.
-                    <span className="chat-timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-
-                <p className="chat-message chat-reciever">
-                    <span className="chat-name">Debjit</span>
-                    This is message.
-                    <span className="chat-timestamp">
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
+                <MicIcon/>
             </div>
         </div>
     )
