dd56213a54be1da1b3f325872d1aaca941c687ce
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-17 11:01:39 +0530

    Version 1

--- src/Firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBQoK0zSQi5F1lVL-cSqy5DA_or3VydcP4",
-  authDomain: "whatsapp-mern-clone-156c3.firebaseapp.com",
-  projectId: "whatsapp-mern-clone-156c3",
-  storageBucket: "whatsapp-mern-clone-156c3.appspot.com",
-  messagingSenderId: "981513484646",
-  appId: "1:981513484646:web:f53fd0b2adebc63ed3f0a7"
-};
