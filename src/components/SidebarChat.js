ec4c14938e92c36450d51f2d675dc183a8e95cc6
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-22 14:54:13 +0530

    Version 1

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -1,31 +1,29 @@
-import React , {useEffect,useState} from 'react'
+import React , {} from 'react'
 import {Avatar} from "@material-ui/core"
 import "../styles/SidebarChat.css"
 import {Link} from 'react-router-dom'
-import axios from '../Axios'
 
 const SidebarChat = ({id,name,image}) => {
 
-    const [messages,setMessages] = useState([]);
 
-     useEffect(() => {
-        axios.get('/messages/sync')
-        .then(res => {
-            setMessages(res.data);
-        })
-    }, [])
+    
 
     return(
+        <>
 
         <Link to={`/rooms/${id}`}>
             <div className="sidebarChat">
                 <Avatar src={image}/>
                 <div className="sidebarChat-info">
                     <h2>{name}</h2>
-                    <p>{messages[messages.length - 1].message}</p>
                 </div>
             </div>
         </Link>
+
+
+        </>
+
+        
         
     );
 }
