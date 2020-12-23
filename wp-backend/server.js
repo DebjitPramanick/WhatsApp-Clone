1964e46404cc919f194bfb3e8556838d516af093
Debjit Pramanick <73888326+DebjitPramanick@users.noreply.github.com>
2020-12-24 00:13:17 +0530

    Update server.js

--- wp-backend/server.js
+++ wp-backend/server.js
@@ -151,12 +151,4 @@ app.post('/rooms/new',(req,res) => {
 
 //Listen
 
-if(process.env.NODE_ENV == "production"){
-    app.use(express.static("build"))
-    const path = require('path')
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'build','index.html'))
-    })
-}
-
 app.listen(port,()=>console.log(`Listening on local host:${port}`))
