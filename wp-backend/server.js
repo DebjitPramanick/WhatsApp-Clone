64077db3ed5e6c7f2410666124f8feaff0d9cc38
DebjitPramanick <dpramanick9990@gmail.com>
2020-12-23 16:53:53 +0530

    Version 1

--- wp-backend/server.js
+++ wp-backend/server.js
@@ -148,6 +148,15 @@ app.post('/rooms/new',(req,res) => {
 })
 
 
+
 //Listen
+
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("build"))
+    const path = require('path')
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'build','index.html'))
+    })
+}
 
 app.listen(port,()=>console.log(`Listening on local host:${port}`))
